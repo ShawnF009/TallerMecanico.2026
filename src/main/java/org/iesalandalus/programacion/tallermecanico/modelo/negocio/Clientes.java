f9936fe7547ea3eab8445a3b66bfdcf9e0521e20
Antonio Manuel Cadenas Ruzafa <mtrvamcr@gmail.com>
2026-03-17 14:39:20 +0100

    Dia 3

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Clientes.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Clientes.java
@@ -1,4 +1,35 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
 
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Clientes {
+
+    private List<Cliente> coleccionClientes;
+
+    public Clientes(){
+        this.coleccionClientes = new ArrayList<>();
+    }
+
+    public List<Cliente> get(){
+        return coleccionClientes;
+    }
+    public void insertar(Cliente cliente){
+            Objects.requireNonNull(cliente, "No se puede insertar un cliente nulo.");
+
+    }
+    public Cliente modificar(Cliente cliente, String nombre, String telefono){
+        Objects.requireNonNull(cliente, "No se puede modificar un cliente nulo.");
+        return cliente;
+    }
+    public Cliente buscar(Cliente cliente){
+        Objects.requireNonNull(cliente, "No se puede buscar un cliente nulo.");
+        return cliente;
+    }
+    public void borrar(Cliente cliente){
+        Objects.requireNonNull(cliente, "No se puede borrar un cliente nulo.");
+    }
 }
