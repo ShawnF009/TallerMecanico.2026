be63304b36987759421d3d288fd71ff56cec7668
Antonio Manuel Cadenas Ruzafa <mtrvamcr@gmail.com>
2026-04-29 14:21:17 +0200

    Dia 3

--- src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
@@ -1,7 +1,10 @@
 package org.iesalandalus.programacion.tallermecanico;
 
+import org.iesalandalus.programacion.tallermecanico.modelo.FabricaModelo;
+import org.iesalandalus.programacion.tallermecanico.modelo.Modelo;
+
 public class Main {
     public static void main(String[] args) {
-
+        Modelo modelo = FabricaModelo
     }
 }
